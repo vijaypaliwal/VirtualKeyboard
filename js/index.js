711ee5e9b10c4433d3500b5db28a169d12ebe1ff
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-02-09 18:28:36 +0530

    latest

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
+var appnew = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
