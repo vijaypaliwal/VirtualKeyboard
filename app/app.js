bf4addf62d9b6179977f62a19154005804261cfb
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-02-09 18:00:38 +0530

    latest

--- app/app.js
+++ app/app.js
@@ -5,37 +5,37 @@ app.config(function ($routeProvider) {
 
     $routeProvider.when("/home", {
         controller: "homeController",
-        templateUrl: "/scanner/app/views/home.html"
+        templateUrl: "/app/views/home.html"
     });
 
     $routeProvider.when("/login", {
         controller: "loginController",
-        templateUrl: "/scanner/app/views/login.html"
+        templateUrl: "/app/views/login.html"
     });
 
     $routeProvider.when("/signup", {
         controller: "signupController",
-        templateUrl: "/scanner/app/views/signup.html"
+        templateUrl: "/app/views/signup.html"
     });
 
     $routeProvider.when("/orders", {
         controller: "ordersController",
-        templateUrl: "/scanner/app/views/orders.html"
+        templateUrl: "/app/views/orders.html"
     });
 
     $routeProvider.when("/refresh", {
         controller: "refreshController",
-        templateUrl: "/scanner/app/views/refresh.html"
+        templateUrl: "/app/views/refresh.html"
     });
 
     $routeProvider.when("/tokens", {
         controller: "tokensManagerController",
-        templateUrl: "/scanner/app/views/tokens.html"
+        templateUrl: "/app/views/tokens.html"
     });
 
     $routeProvider.when("/associate", {
         controller: "associateController",
-        templateUrl: "/scanner/app/views/associate.html"
+        templateUrl: "/app/views/associate.html"
     });
 
     $routeProvider.otherwise({ redirectTo: "/home" });
