85b4ca92921dc8ea99f62071af696cc6d5885f17
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-02-09 17:50:16 +0530

    Today's update

---
+++ app/controllers/ordersController.js
@@ -0,0 +1,44 @@
+﻿'use strict';
+app.controller('ordersController', ['$scope', 'ordersService', function ($scope, ordersService) {
+
+    $scope.orders = [];
+    $scope.scannerText = "";
+
+    //ordersService.getOrders().then(function (results) {
+
+    //    $scope.orders = results.data;
+
+    //}, function (error) {
+    //    //alert(error.data.message);
+    //});
+
+    $scope.Scan=function()
+    {
+        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+        scanner.scan(function (result) {
+
+
+
+            $scope.scannerText = result.text;
+            
+
+
+           
+            console.log("Scanner result: \n" +
+                 "text: " + result.text + "\n" +
+                 "format: " + result.format + "\n" +
+                 "cancelled: " + result.cancelled + "\n");
+            
+            /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+
+        }, function (error) {
+            console.log("Scanning failed: ", error);
+        });
+    }
+
+}]);
