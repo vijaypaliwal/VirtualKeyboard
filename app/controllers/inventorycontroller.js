598d402152b8b0e853d8a480c97c170d13250648
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-06-03 11:54:15 +0530

    test

--- app/controllers/inventorycontroller.js
+++ app/controllers/inventorycontroller.js
@@ -1312,6 +1312,7 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
 
     $scope.handleFileSelect = function (evt) {
 
+
         debugger;
         var files = evt.target.files;
         FileName = "";
@@ -1330,13 +1331,36 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
             // Closure to capture the file information.
             reader.onload = (function (theFile) {
 
+
+                var view = new DataView(theFile.target.result);
+                if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+                var length = view.byteLength, offset = 2;
+                while (offset < length) {
+                    var marker = view.getUint16(offset, false);
+                    offset += 2;
+                    if (marker == 0xFFE1) {
+                        if (view.getUint32(offset += 2, false) != 0x45786966) return callback(-1);
+                        var little = view.getUint16(offset += 6, false) == 0x4949;
+                        offset += view.getUint32(offset + 4, little);
+                        var tags = view.getUint16(offset, little);
+                        offset += 2;
+                        for (var i = 0; i < tags; i++)
+                            if (view.getUint16(offset + (i * 12), little) == 0x0112)
+                                return callback(view.getUint16(offset + (i * 12) + 8, little));
+                    }
+                    else if ((marker & 0xFF00) != 0xFF00) break;
+                    else offset += view.getUint16(offset, false);
+                }
+
+
+
                 var id = randomString(5, '0123456789');
                 _ImgObj.ImageID = id;
 
                 var crossicon = '<a class="btn btn-danger removeImage" altid="' + id + '" onclick="removeImage(' + id + ')"><i class="fa fa-times"></i></a>';
                 var compilehtml = $compile(crossicon)($scope);
 
-
+               
 
 
                 return function (e) {
