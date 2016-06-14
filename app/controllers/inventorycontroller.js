b16d6fc3e85528a2c65fafe9cd5b2a5e82c3c624
vijaypaliwal <vijay.paliwal@hotmail.com>
2016-06-14 10:57:38 +0530

    test

--- app/controllers/inventorycontroller.js
+++ app/controllers/inventorycontroller.js
@@ -1278,7 +1278,9 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
     }
 
     $scope.OpenBox = function () {
+        alert("Click");
         $("#files").trigger("click");
+        alert("Clickafter");
     }
 
     $scope.triggerFileClick = function () {
