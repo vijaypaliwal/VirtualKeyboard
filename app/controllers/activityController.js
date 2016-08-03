4775fb7a85af421805d0e9dcf39b42d339167aad
ajaypaliwal <ajaypaliwal.cto@Outlook.com>
2016-08-03 18:38:13 +0530

    minor change for date
    minor change for date

--- app/controllers/activityController.js
+++ app/controllers/activityController.js
@@ -1902,7 +1902,7 @@ app.controller('activityController', ['$scope', 'localStorageService', 'authServ
         var dsplit1 = _updateDateval.split("-");
 
         var d122 = new Date(dsplit1[0], dsplit1[1] - 1, dsplit1[2]);
-
+        d122.setDate(d122.getDate() + 1);
         var d112 = new Date(Date.UTC(d122.getFullYear(), d122.getMonth(), d122.getDate()-1, 0, 0, 0, 0))
 
         wcfDateStr123 = d122.toMSJSON();
@@ -2056,7 +2056,7 @@ app.controller('activityController', ['$scope', 'localStorageService', 'authServ
                 var dsplit = dateVar.indexOf("/") == -1 ? dateVar.split("-") : dateVar.split("/");
 
                 var d1 = dateVar.indexOf("/") == -1 ? new Date(dsplit[0], dsplit[1] - 1, dsplit[2]) : new Date(dsplit[2], dsplit[1] - 1, dsplit[0]);
-
+                d1.setDate(d1.getDate() + 1);
                 var d11 = new Date(Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate()-1, 0, 0, 0, 0))
 
                 wcfDateStr1 = d11.toMSJSON();
@@ -2067,7 +2067,7 @@ app.controller('activityController', ['$scope', 'localStorageService', 'authServ
 
 
                 var d2 = dateVar.indexOf("/") == -1 ? new Date(dsplit[0], dsplit[1] - 1, dsplit[2]) : new Date(dsplit[2], dsplit[1] - 1, dsplit[0]);
-
+                d2.setDate(d2.getDate() + 1);
                 var d21 = new Date(Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate()-1, 0, 0, 0, 0))
 
                 wcfDateStr2 = d21.toMSJSON();
