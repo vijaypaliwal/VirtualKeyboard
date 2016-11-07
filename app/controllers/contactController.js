b8e6de0709cb2bfeab80ba603f357e694fa14708
dashrath123 <dashrath.k@shivamitconsultancy.com>
2016-11-07 16:16:12 +0530

    added map
    added map

--- app/controllers/contactController.js
+++ app/controllers/contactController.js
@@ -7,4 +7,33 @@ app.controller('contactController', ['$scope', '$location', 'authService', 'ngAu
     $(".modal-backdrop").remove();
     $("body").removeClass("modal-open");
 
+
+    var myCenter = new google.maps.LatLng(38.5737822, -90.3928599);
+
+    function initialize() {
+
+        var mapProp = {
+            center: myCenter,
+            zoom: 17,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+        var marker = new google.maps.Marker({
+            position: myCenter,
+        });
+
+        marker.setMap(map);
+
+        var infowindow = new google.maps.InfoWindow({
+            content: "Clearly Inventory"
+        });
+
+        infowindow.open(map, marker);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    initialize();
+
 }]);
