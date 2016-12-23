de92aa854a0e5e29d2861bd82c9f290316d9874e
shivamgautampaliwal <gautam.p@shivamitconsultancy.com>
2016-12-23 16:21:13 +0530

    Latest code for IOS
    Latest code for IOS

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -4,6 +4,33 @@ app.controller('dashboardController', ['$scope', 'localStorageService', 'authSer
 
     $scope.UserName = "";
     $scope.logintext = false;
+    $scope.Inventories = [];
+    $scope.InventoryName = "";
+    $scope.masterdataloaded = false;
+    $scope.Inventory={ InventoryID :6,
+
+      InventoryName:"",  
+      Owner:$scope.UserName,  
+      ItemsUsed:4000 , 
+      ItemsAvailable:4600  ,
+      LocationsUsed:1200,  
+
+      LocationsAvailable:1500  ,
+      TotalUsers:0  ,
+
+      Users:[] ,
+
+      API:false,
+
+      Integrations :"" ,
+
+      InventoryType:1,
+
+      IsActive:true, 
+
+      AccountID: 4}
+
+    $scope.Users = [];
     $scope.GetDataByUserID = function (ID) {
         debugger;
         $scope.UserName = "";
@@ -37,6 +64,59 @@ app.controller('dashboardController', ['$scope', 'localStorageService', 'authSer
     }
 
 
+    $scope.GetCurrentClass = function (ID) {
+        debugger;
+        $scope.CurrentCSS = "";
+        switch (ID) {
+            case 1:
+                return "bgm-black";
+
+
+                break;
+            case 2:
+                return "bgm-gold";
+                break;
+            case 3:
+                return "bgm-pink";
+                break;
+            case 4:
+                return  "bgm-purple";
+                break;
+            case 5:
+                return "bgm-blue";
+                break;
+            case 6:
+                return "bgm-lightblue";
+                break;
+            default:
+                return "bgm-red";
+
+        }
+
+
+ 
+    }
+
+    $scope.PushInventory=function()
+    {
+        debugger;
+        var _Obj = angular.copy($scope.Inventory);
+        _Obj.InventoryName = $scope.InventoryName;
+        $scope.Inventories.push(_Obj);
+        $("#Addinvmodal").modal('hide');
+
+        CheckScopeBeforeApply();
+    }
+
+    $scope.addinventory = function () {
+        
+        $("#bottommenumodal").modal('hide');
+
+        $("#Addinvmodal").modal('show');
+        CheckScopeBeforeApply();
+    }
+
+
 
     $('#bottommenumodal').on('hidden.bs.modal', function () {
         $(".menubtn .fa").removeClass('fa-times').addClass('fa-bars');
@@ -66,28 +146,30 @@ app.controller('dashboardController', ['$scope', 'localStorageService', 'authSer
     };
     var mySwiper;
 
-    mySwiper = new Swiper('.swiper-container', {
-        initialSlide: 0,
-        speed: 500,
-        effect: 'flip',
-        allowSwipeToPrev: false,
-        onSlideChangeEnd: function (swiperHere) {
+    function EnableSwiper() {
 
-            if(swiperHere.activeIndex==0)
-            {
-                $scope.ActiveClass = "left0";
-            }
-            else if (swiperHere.activeIndex == 1) {
-                $scope.ActiveClass = "left33";
-            }
-            else if (swiperHere.activeIndex == 2) {
-                $scope.ActiveClass = "left66";
-            }
-            CheckScopeBeforeApply();
+        mySwiper = new Swiper('.swiper-container', {
+            initialSlide: 0,
+            speed: 500,
+            effect: 'flip',
+            allowSwipeToPrev: false,
+            onSlideChangeEnd: function (swiperHere) {
 
-        }
+                if (swiperHere.activeIndex == 0) {
+                    $scope.ActiveClass = "left0";
+                }
+                else if (swiperHere.activeIndex == 1) {
+                    $scope.ActiveClass = "left33";
+                }
+                else if (swiperHere.activeIndex == 2) {
+                    $scope.ActiveClass = "left66";
+                }
+                CheckScopeBeforeApply();
 
-    });
+            }
+
+        });
+    }
 
     $scope.GotoStep = function (_Step) {
         if(_Step==0)
@@ -111,6 +193,50 @@ app.controller('dashboardController', ['$scope', 'localStorageService', 'authSer
 
     }
 
+    $scope.GoToEditInventory=function(ID)
+    {
+        localStorageService.set("InventoryID", ID);
+        $location.path("/manageinventory");
+    }
+
+
+    $scope.GetInventoryMaster = function () {
+        $scope.masterdataloaded = false;
+        $.ajax
+              ({
+                  type: "POST",
+                  url: serviceBase + 'GetInventoryMasters',
+                  contentType: 'application/json; charset=utf-8',
+                  dataType: 'json',
+                  data: JSON.stringify({ "SecurityToken": $scope.SecurityToken }),
+                  success: function (response) {
+                      $scope.masterdataloaded = true;
+                      if (response.GetInventoryMastersResult.Success == true) {
+                          $scope.Inventories = response.GetInventoryMastersResult.Payload;
+                          $scope.Users = $scope.Inventories[0].Users;
+                          localStorageService.set("Inventories", $scope.Inventories);
+                          CheckScopeBeforeApply()
+                      }
+                      else {
+
+                          $scope.ShowErrorMessage("GetInventoryMasters", 1, 1, response.SendEmailResult.Message);
+
+                      }
+
+                  },
+                  error: function (err) {
+
+                      $scope.errorbox(err);
+                      $scope.ShowErrorMessage("GetInventoryMasters", 2, 1, err.statusText);
+
+                  },
+                  complete:function()
+                  {
+                      EnableSwiper();
+                  }
+              });
+    }
+
     function init()
     {
         debugger;
@@ -118,7 +244,7 @@ app.controller('dashboardController', ['$scope', 'localStorageService', 'authSer
         $scope.CurrentUserID = parseInt(_value);
         $scope.GetDataByUserID($scope.CurrentUserID);
         $scope.logintext = true;
-
+        $scope.GetInventoryMaster();
         setTimeout(function() {
             $(".loginusername").fadeOut();
             CheckScopeBeforeApply();
--- app/controllers/manageinventoryController.js
+++ app/controllers/manageinventoryController.js
@@ -4,11 +4,26 @@ app.controller('manageinventoryController', ['$scope', 'localStorageService', 'a
     $scope.Isinventory = false;
     $scope.Isfulluser = false;
     $scope.Isviewuser = false;
+    $scope.LocalInventories = [];
+    $scope.selectedInventory = {};
+    $scope.LocalInvID = 1;
     function CheckScopeBeforeApply() {
         if (!$scope.$$phase) {
             $scope.$apply();
         }
     };
+
+    $("#selectInv").change(function () {
+
+        if ($.trim($(this).val()) != "") {
+            $scope.GetSelectedInventory(parseInt($(this).val()));
+
+        }
+        else {
+            $scope.selectedInventory = {};
+        }
+        CheckScopeBeforeApply();
+    });
     $scope.openClosePanel = function (Type) {
         switch (Type) {
             case 1:
@@ -18,15 +33,16 @@ app.controller('manageinventoryController', ['$scope', 'localStorageService', 'a
                 break;
 
             case 2:
-                $scope.Isviewuser = false;
+                $scope.Isfulluser = false;
                 $scope.Isinventory = false;
-                $scope.Isfulluser = !$scope.Isfulluser;
+                $scope.Isviewuser = !$scope.Isviewuser;
                 break;
 
             case 3:
                 $scope.Isinventory = false;
-                $scope.Isviewuser = !$scope.Isviewuser;
-                $scope.Isfulluser = false;
+                $scope.Isfulluser = !$scope.Isfulluser;
+
+                $scope.Isviewuser = false;
                 break;
 
            
@@ -37,4 +53,35 @@ app.controller('manageinventoryController', ['$scope', 'localStorageService', 'a
     }
   
     $scope.openClosePanel(1);
+    $scope.GetSelectedInventory = function (ID) {
+        debugger;
+        for (var i = 0; i < $scope.LocalInventories.length; i++) {
+            if ($scope.LocalInventories[i].InventoryID == ID) {
+                $scope.selectedInventory = $scope.LocalInventories[i];
+                break;
+            }
+
+        }
+
+        CheckScopeBeforeApply();
+    }
+    function init()
+    {
+        debugger;
+        $scope.LocalInventories = localStorageService.get("Inventories");
+        var _LocalInvID = parseInt(localStorageService.get("InventoryID"));
+        if (_LocalInvID == null || _LocalInvID == undefined)
+        {
+            _LocalInvID = 1;
+        }
+        $scope.LocalInvID = _LocalInvID;
+        setTimeout(function () {
+            $("#selectInv").val(_LocalInvID);
+            $scope.GetSelectedInventory(_LocalInvID);
+        }, 100);
+        
+        CheckScopeBeforeApply();
+    }
+
+    init();
 }]);
