4b047bbba193a30f90c39e2dbea0497b02059c78
ajaypaliwal <ajaypaliwal.cto@outlook.com>
2017-03-06 17:25:21 +0530

    asdfasd
    asdf

--- app/controllers/activityController.js
+++ app/controllers/activityController.js
@@ -2251,6 +2251,10 @@ app.controller('activityController', ['$scope', 'localStorageService', 'authServ
                                    }
                                }
                            }
+                           else if ($scope.CustomActivityDataList[i].cfdDataType == "checkbox") {
+                               var _value = angular.copy($scope.CustomActivityDataList[i].cfdDefaultValue);
+                               $scope.CustomActivityDataList[i].cfdDefaultValue = (_value == "true" || _value === "True");
+                           }
                        }
 
                        CheckScopeBeforeApply();
