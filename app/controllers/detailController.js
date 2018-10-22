65e3b272a4ed36ecd8785b5fd869395654efd3d9
shivamgautampaliwal <gautam.p@shivamitconsultancy.com>
2018-10-22 15:29:05 +0530

    Changes.
    Changes.

--- app/controllers/detailController.js
+++ app/controllers/detailController.js
@@ -229,7 +229,7 @@ app.controller('detailController', ['$scope', 'localStorageService', 'authServic
       
         navigator.camera.getPicture($scope.onPhotoURISuccessNew, $scope.onFail, {
             quality: 50,
-            destinationType: destinationType.FILE_URI,
+            destinationType: destinationType.NATIVE_URI,
             allowEdit: true,
             correctOrientation: true,
             sourceType: pictureSource.PHOTOLIBRARY
