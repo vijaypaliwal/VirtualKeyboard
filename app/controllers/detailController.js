60b274fb481ddb5412fa9e2856c446872a745934
shivamgautampaliwal <gautam.p@shivamitconsultancy.com>
2018-10-22 14:33:01 +0530

    Image Name Changes.
    Image Name Changes.

--- app/controllers/detailController.js
+++ app/controllers/detailController.js
@@ -229,7 +229,7 @@ app.controller('detailController', ['$scope', 'localStorageService', 'authServic
       
         navigator.camera.getPicture($scope.onPhotoURISuccessNew, $scope.onFail, {
             quality: 50,
-            destinationType: destinationType.DATA_URL,
+            destinationType: destinationType.FILE_URI,
             allowEdit: true,
             correctOrientation: true,
             sourceType: pictureSource.PHOTOLIBRARY
